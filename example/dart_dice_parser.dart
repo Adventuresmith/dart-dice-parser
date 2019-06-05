f3010f2816958ab28ccbf83eac928d0c11b659e5
Steve Christensen <steve.christensen@mediciventures.com>
2019-06-05 03:55:23 -0600

    cleanup example output

--- example/dart_dice_parser.dart
+++ example/dart_dice_parser.dart
@@ -22,17 +22,17 @@ int roll(int numRolls, String expression) {
     print("Supply a dice expression. e.g. '2d6+1'");
     return 1;
   }
-  print("Evaluating: $expression\n");
   var diceParser = new DiceParser();
 
   var result = diceParser.parse(expression);
   if (result.isFailure) {
-    print(expression);
-    print('${' ' * (result.position - 1)}^-- ${result.message}');
+    print("Failure:");
+    print('\t' + expression);
+    print('\t${' ' * (result.position - 1)}^-- ${result.message}');
     return 1;
   }
   // use the parser to display parse results
-  print("\t ${result}\n");
+  print("Evaluating: $expression => ${result}\n");
   // but use the evaluator via roll/rollN to actually parse and perform dice roll
   diceParser
       .rollN(expression, numRolls)
