3806f2d0fb35ba3ccf88597ad13bb38fe8e836bb
Steve Christensen <stevec1973@gmail.com>
2022-10-14 21:39:25 -0600

    clean up tests and make it use new diceexpression implementation

--- test/dart_dice_parser_test.dart
+++ test/dart_dice_parser_test.dart
@@ -7,9 +7,10 @@ import 'package:test/test.dart';
 class MockRandom extends Mock implements Random {}
 
 void main() {
-  // dice parser whose random always returns '1' (rolls '2')
-  late DiceParser staticDiceParser;
-  late DiceParser seededDiceParser;
+  // dice parser whose random always returns '1' (aka roll will always be '2')
+  late DiceExpressionFactory staticFactory;
+
+  late DiceExpressionFactory seededFactory;
 
   setUp(() async {
     // first 100 seeded rolls for d6
@@ -22,271 +23,125 @@ void main() {
       () => staticMockRandom.nextInt(any()),
     ).thenReturn(1);
 
-    staticDiceParser = DiceParser(staticMockRandom);
-    seededDiceParser = DiceParser(seededRandom);
+    staticFactory = DiceExpressionFactory(staticMockRandom);
+    seededFactory = DiceExpressionFactory(seededRandom);
   });
+  void staticRandTest(String name, String input, int expected) {
+    test("$name - $input", () {
+      expect(staticFactory.create(input).roll(), equals(expected));
+    });
+  }
+
+  void seededRandTest(String name, String input, int expected) {
+    test("$name - $input", () {
+      expect(seededFactory.create(input).roll(), equals(expected));
+    });
+  }
 
   group("arithmetic", () {
-    test("addition", () {
-      expect(staticDiceParser.roll("1 + 20"), equals(21));
-    });
-
-    test("multi", () {
-      expect(staticDiceParser.roll("3 * 2"), equals(6));
-    });
-
-    test("parens", () {
-      expect(staticDiceParser.roll("(5 + 6) * 2"), equals(22));
-    });
-
-    test("order of operations", () {
-      expect(staticDiceParser.roll("5 + 6 * 2"), equals(17));
-    });
+    staticRandTest("addition", "1+20", 21);
+    staticRandTest("multi", "3*2", 6);
+    staticRandTest("parens", "(5+6)*2", 22);
+    staticRandTest("order of operations", "5+6*2", 17);
+  });
+  group("debug", () {
+    seededRandTest("count =", "4d6#=1", 1);
   });
 
   group("counting operations", () {
-    test("count >", () {
-      // mocked responses should return rolls of 6, 2, 1, 5
-      //
-      expect(seededDiceParser.roll("4d6#>3"), equals(2));
-    });
-    test("count <", () {
-      // mocked responses should return rolls of 6, 2, 1, 5
-      //
-      expect(seededDiceParser.roll("4d6#<4"), equals(2));
-    });
-    test("count =", () {
-      // mocked responses should return rolls of 6, 2, 1, 5
-      //
-      expect(seededDiceParser.roll("4d6#=1"), equals(1));
-    });
-    test("count > (missing)", () {
-      // mocked responses should return rolls of 6, 2, 1, 5
-      //
-      expect(seededDiceParser.roll("4d6#>6"), equals(0));
-    });
-
-    test("count #", () {
-      // mocked responses should return rolls of 6, 2, 1, 5
-      //
-      expect(seededDiceParser.roll("4d6#"), equals(4));
-    });
-    test("count # after drop", () {
-      // mocked responses should return rolls of 6, 2, 1, 5
-      //
-      expect(seededDiceParser.roll("4d6-<2#"), equals(3));
-    });
+    // mocked responses should return rolls of 6, 2, 1, 5
+    seededRandTest("count >", "4d6#>3", 2);
+    seededRandTest("count <", "4d6#>1", 3);
+    seededRandTest("count =", "4d6#=1", 1);
+    seededRandTest("count > (missing from result)", "4d6#>6", 0);
+    seededRandTest("count #", "4d6#", 4);
+    seededRandTest("count # after drop", "4d6-<2#", 3);
   });
   group("roll modifiers - drop, clamp, etc", () {
-    test("drop high", () {
-      // mocked responses should return rolls of 6, 2, 1, 5
-      //
-      expect(seededDiceParser.roll("4d6-H"), equals(8));
-    });
-    test("drop high (lowercase)", () {
-      // mocked responses should return rolls of 6, 2, 1, 5
-      //
-      expect(seededDiceParser.roll("4d6-h"), equals(8));
-    });
-    test("drop high - 1", () {
-      // equivalent to the above
-      expect(seededDiceParser.roll("4d6-H1"), equals(8));
-    });
-    test("drop high - 3", () {
-      // mocked responses should return rolls of 6, 2, 1, 5
-      //
-      expect(seededDiceParser.roll("4d6-H3"), equals(1));
-    });
-    test("drop low", () {
-      // mocked responses should return rolls of 6, 2, 1, 5
-      //
-      expect(seededDiceParser.roll("4d6-L"), equals(13));
-    });
+    // mocked responses should return rolls of 6, 2, 1, 5
+    seededRandTest("drop high", "4d6-H", 8);
+    seededRandTest("drop high (lowercase)", "4d6-h", 8);
+    seededRandTest("drop high (1)", "4d6-h1", 8);
+    seededRandTest("drop high (3)", "4d6-h3", 1);
+    seededRandTest("drop low", "4d6-L", 13);
+    seededRandTest("drop low (lower)", "4d6-l", 13);
+    seededRandTest("drop low - 1", "4d6-l1", 13);
+    seededRandTest("drop low - 3", "4d6-l3", 6);
+    seededRandTest("drop low and high", "4d6-L-H", 7);
+    seededRandTest("dropping an int drops all", "4-L3", 0);
+    seededRandTest("can drop more than rolled", "3d6-H4", 0);
+    seededRandTest("can drop more than rolled", "3d6-l4", 0);
+    seededRandTest("drop >3", "4d6->3", 3);
+    seededRandTest("drop <3", "4d6-<3", 11);
+    seededRandTest("drop =2", "4d6-=2", 12);
+    seededRandTest("drop =4 (not in results)", "4d6-=4", 14);
+    seededRandTest("clamp > 3", "4d6C>3", 9);
+    seededRandTest("clamp < 3", "4d6C<3", 17);
+    seededRandTest("clamp > 3 lowercase", "4d6c>3", 9);
+    seededRandTest("clamp < 3 lowercase", "4d6c<3", 17);
 
-    test("drop low (lowercase)", () {
-      // mocked responses should return rolls of 6, 2, 1, 5
-      //
-      expect(seededDiceParser.roll("4d6-l"), equals(13));
-    });
-    test("drop low - 1", () {
-      // mocked responses should return rolls of 6, 2, 1, 5
-      //
-      expect(seededDiceParser.roll("4d6-L1"), equals(13));
-    });
-    test("drop low and high", () {
-      // mocked responses should return rolls of 6, 2, 1, 5
-      //
-      expect(seededDiceParser.roll("4d6-L-H"), equals(7));
-    });
-    test("drop low - 3", () {
-      // mocked responses should return rolls of 6, 2, 1, 5
-      //
-      expect(seededDiceParser.roll("4d6-L3"), equals(6));
-    });
-    test("drop low on aggregated dice", () {
-      // mocked responses should return rolls of 6, 2, 1, 5, 3
-      // [6,2] + [1,5,3] = [6,2,1,5,3]-L3 => [6,5] = 9
-      expect(seededDiceParser.roll("(2d6+3d6)-L3"), equals(11));
-    });
-    test("drop on int drops", () {
-      expect(seededDiceParser.roll("4-L3"), equals(0));
-    });
-    test("can drop more than rolled", () {
-      expect(seededDiceParser.roll("3d6-H4"), equals(0));
-    });
-    test("can drop more than rolled (low)", () {
-      expect(seededDiceParser.roll("3d6-L4"), equals(0));
-    });
-    test("drop >3", () {
-      // mocked responses should return rolls of 6, 2, 1, 5
-      //
-      expect(seededDiceParser.roll("4d6->3"), equals(3));
-    });
-    test("drop <3", () {
-      // mocked responses should return rolls of 6, 2, 1, 5
-      //
-      expect(seededDiceParser.roll("4d6-<3"), equals(11));
-    });
-    test("drop =2", () {
-      // mocked responses should return rolls of 6, 2, 1, 5
-      //
-      expect(seededDiceParser.roll("4d6-=2"), equals(12));
-    });
-    test("clamp >3", () {
-      // mocked responses should return rolls of 6, 2, 1, 5
-      //
-      expect(seededDiceParser.roll("4d6C>3"), equals(9));
-    });
-    test("clamp <3", () {
-      // mocked responses should return rolls of 6, 2, 1, 5
-      //
-      expect(seededDiceParser.roll("4d6C<3"), equals(17));
-    });
-    test("clamp >3 (lowercase)", () {
-      // mocked responses should return rolls of 6, 2, 1, 5
-      //
-      expect(seededDiceParser.roll("4d6c>3"), equals(9));
-    });
-    test("clamp <3 (lowercase)", () {
-      // mocked responses should return rolls of 6, 2, 1, 5
-      //
-      expect(seededDiceParser.roll("4d6c<3"), equals(17));
-    });
+    // mocked responses should return rolls of 6, 2, 1, 5, 3
+    // [6,2] + [1,5,3] = [6,2,1,5,3]-L3 => [6,5] = 9
+    seededRandTest("drop low on aggregated dice", "(2d6+3d6)-L3", 11);
   });
   group("addition combines", () {
-    test("addition combines results (drop is higher order than plus)", () {
-      // mocked responses should return rolls of 6, 2, 1, 5
-      expect(seededDiceParser.roll("3d6+1d6-L1"), equals(9));
-    });
-    test("addition combines results - parens", () {
-      // mocked responses should return rolls of 6, 2, 1, 5
-      expect(seededDiceParser.roll("(2d6+2d6)-L1"), equals(13));
-    });
-    test("addition combines results", () {
-      // mocked responses should return rolls of 6, 2, 1, 5
-      expect(seededDiceParser.roll("(2d6+1)-L1"), equals(8));
-    });
+    // mocked responses should return rolls of 6, 2, 1, 5
+    seededRandTest("addition combines results (drop is higher order than plus)",
+        "3d6+1d6-L1", 9);
+    seededRandTest("addition combines results - parens", "(2d6+2d6)-L1", 13);
+    seededRandTest("addition combines results", "(2d6+1)-L1", 8);
   });
 
   group("mult variations", () {
-    test("int mult on rhs", () {
-      // mocked responses should return rolls of 6, 2, 1, 5
-      expect(seededDiceParser.roll("2d6*2"), equals(16));
-    });
-    test("int mult on lhs", () {
-      // mocked responses should return rolls of 6, 2, 1, 5
-      expect(seededDiceParser.roll("2*2d6"), equals(16));
-    });
-    test("dropped multiplied results", () {
-      // mocked responses should return rolls of 6, 2, 1, 5
-      expect(seededDiceParser.roll("(2d6*2)-L"), equals(12));
-    });
+    // mocked responses should return rolls of 6, 2, 1, 5
+    seededRandTest("int mult on rhs", "2d6*2", 16);
+    seededRandTest("int mult on lhs", "2*2d6", 16);
+    // mult collapses into single result, so dropping will wind up w/ zero return
+    seededRandTest("dropped multiplied results", "(2d6*2)-L", 0);
   });
   group("missing ints", () {
-    test("empty string returns zero", () {
-      expect(staticDiceParser.roll(""), equals(0));
-    });
-    test("empty arith returns zero - add", () {
-      expect(staticDiceParser.roll("+"), equals(0));
-    });
-    test("empty arith returns zero - mult", () {
-      expect(staticDiceParser.roll("*"), equals(0));
-    });
-    test("empty ndice is 1", () {
-      expect(staticDiceParser.roll("d6"), equals(2));
-    });
-    test("empty nsides is 1", () {
-      // weird because mocked random returns '2'
-      expect(staticDiceParser.roll("6d"), equals(12));
-    });
-    test("whitespace should be swallowed", () {
-      expect(staticDiceParser.roll("2 d6"), equals(4));
-      expect(staticDiceParser.roll("2d 6"), equals(4));
-    });
+    staticRandTest("empty string returns zero", "", 0);
+    staticRandTest("empty arith returns zero - add", "+", 0);
+    staticRandTest("empty arith returns zero - mult", "*", 0);
+    staticRandTest("empty ndice is 1", "d6", 2);
+    staticRandTest("whitespace should be swallowed", "2 d6", 4);
+    staticRandTest("whitespace should be swallowed", "2d 6", 4);
+    // weird because mocked random returns '2'
+    staticRandTest("empty nsides is 1", "6d", 12);
   });
 
   group("dice", () {
-    test("order of operations, with dice", () {
-      // multiply first, then add
-      expect(staticDiceParser.roll("5 + 6 * 2d6"), equals(29));
-    });
+    staticRandTest("order of operations, with dice", "5 + 6 * 2d6", 29);
 
-    test("simple roll", () {
-      expect(staticDiceParser.roll("1d6"), equals(2));
-    });
-    test("percentile", () {
-      expect(staticDiceParser.roll("1d%"), equals(2));
-    });
-    test("D66", () {
-      expect(staticDiceParser.roll("1D66"), equals(22));
-    });
-    test("d66 -- 66-sided, not D66", () {
-      expect(staticDiceParser.roll("1d66"), equals(2));
-    });
+    staticRandTest("simple roll", "1d6", 2);
+    staticRandTest("percentile", "1d%", 2);
+    staticRandTest("D66", "1D66", 22);
+    staticRandTest("d66 -- 66-sided, not D66", "1d66", 2);
+    staticRandTest("ndice in parens", "(4+6)d10", 20);
+    staticRandTest("nsides in parens", "10d(2*3)", 20);
 
-    test("ndice in parens", () {
-      expect(staticDiceParser.roll("(4+6)d10"), equals(20));
-    });
+    staticRandTest("zero dice rolled", "0d6", 0);
 
-    test("nsides in parens", () {
-      expect(staticDiceParser.roll("10d(2*3)"), equals(20));
-    });
+    staticRandTest("dice expr as sides", "2d(3d6)", 4);
 
-    test("zero dice rolled", () {
-      expect(staticDiceParser.roll("0d6"), equals(0));
-    });
+    staticRandTest("fudge", "4dF", -4);
+    // 1st roll: 6, 2, 1, 5, 3, 5, 1, 4, 6, (explodes 2) (total 33)
+    // 2nd roll: 5,6 (explodes 1) (total 11)
+    // 3rd roll: 4 (explodes 0) (total 4)
+    seededRandTest("exploding dice", "9d!6", 48);
 
-    test("dice expr as sides", () {
-      expect(staticDiceParser.roll("2d(3d6)"), equals(4));
-    });
+    // explode, then count 6's
+    seededRandTest("exploding dice and count", "9d!6#=6", 3);
+    // explode, then drop less-than-6, then count (should be identical to above)
+    seededRandTest("exploding dice and count variation", "9d!6-<6#", 3);
 
-    test("fudge", () {
-      expect(staticDiceParser.roll("4dF"), equals(-4));
-    });
-    test("exploding dice", () {
-      // 1st roll: 6, 2, 1, 5, 3, 5, 1, 4, 6, (explodes 2) (total 33)
-      // 2nd roll: 5,6 (explodes 1) (total 11)
-      // 3rd roll: 4 (explodes 0) (total 4)
-      expect(seededDiceParser.roll("9d!6"), equals(48));
-    });
-
-    test("exploding dice and count", () {
-      // explode, then count 6's
-      expect(seededDiceParser.roll("9d!6#=6"), equals(3));
-    });
-    test("exploding dice and count variation", () {
-      // explode, then drop less-than-6, then count (should be identical to above)
-      expect(seededDiceParser.roll("9d!6-<6#"), equals(3));
-    });
-
-    test("limited exploding dice", () {
-      // 1st roll: 6, 2, 1, 5, 3, 5, 1, 4, 6, (explodes 2) (total 33)
-      // 2nd roll: 5,6 (a six, but shouldn't explode) (total 11)
-      expect(seededDiceParser.roll("9d!!6"), equals(44));
-    });
+    // 1st roll: 6, 2, 1, 5, 3, 5, 1, 4, 6, (explodes 2) (total 33)
+    // 2nd roll: 5,6 (a six, but shouldn't explode) (total 11)
+    seededRandTest("limited exploding dice", "9d!!6", 44);
 
     test("invalid dice str", () {
-      expect(() => staticDiceParser.roll("1d5 + x2"), throwsFormatException);
+      expect(
+          () => seededFactory.create("1d5 + x2").roll(), throwsFormatException);
     });
   });
 }
