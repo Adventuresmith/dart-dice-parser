c50285f5e9c0ddb4abab5a25949263f3e7135d4a
Steve Christensen <stevec1973@gmail.com>
2025-03-18 21:13:05 -0600

    fix analysis error

--- test/dart_dice_parser_test.dart
+++ test/dart_dice_parser_test.dart
@@ -10,7 +10,7 @@ void main() {
   late Random staticMockRandom;
   late Random seededRandom;
 
-  setUp(() async {
+  setUp(() {
     // first 100 seeded rolls for d6
     // [6, 2, 1, 5, 3, 5, 1, 4, 6, 5, 6, 4, 2, 4, 2, 3, 5, 1, 1, 2, 4, 1, 6, 2, 2, 5, 6, 3, 1, 3, 6, 1, 2, 3, 6, 2, 1, 1, 1, 3, 1, 2, 3, 3, 6, 2, 5, 4, 3, 4, 1, 5, 4, 4, 2, 6, 5, 4, 6, 2, 3, 1, 4, 5, 3, 2, 2, 6, 6, 4, 4, 2, 6, 2, 5, 3, 3, 4, 4, 2, 2, 4, 3, 2, 6, 6, 4, 6, 4, 4, 3, 1, 4, 2, 2, 4, 3, 3, 1, 3]
     seededRandom = Random(1234);
